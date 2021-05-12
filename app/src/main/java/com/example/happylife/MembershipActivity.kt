f5f976c350a094ec5594f0d50563fd6ebe192d50
aerim <aerimforest98@gmail.com>
2021-05-13 02:26:09 +0900

    회원가입 화면 레이아웃 구성

---
+++ app/src/main/java/com/example/happylife/MembershipActivity.kt
@@ -0,0 +1,12 @@
+package com.example.happylife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MembershipActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_membership)
+    }
+}
