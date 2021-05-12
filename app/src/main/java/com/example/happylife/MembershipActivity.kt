dc63878d02739483c1a6e4a4df84e88187d5a2c1
aerim <aerimforest98@gmail.com>
2021-05-13 03:13:08 +0900

    폰트 사이즈 변경, 액티비티 연결

--- app/src/main/java/com/example/happylife/MembershipActivity.kt
+++ app/src/main/java/com/example/happylife/MembershipActivity.kt
@@ -1,12 +1,23 @@
 package com.example.happylife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happylife.databinding.ActivityMembershipBinding
 
 class MembershipActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMembershipBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_membership)
+        binding = ActivityMembershipBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnSigninMembership.setOnClickListener {
+            val intent = Intent(this, NicknameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
