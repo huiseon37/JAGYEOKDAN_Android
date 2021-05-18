e9c555709dcc0c6054cd910afcd3df39476693bb
aerim <aerimforest98@gmail.com>
2021-05-19 05:25:43 +0900

    자격증 등록 레이아웃 구성

--- app/src/main/java/com/example/happylife/RegisterActivity.kt
+++ app/src/main/java/com/example/happylife/RegisterActivity.kt
@@ -1,11 +1,32 @@
 package com.example.happylife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happylife.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tvNicknameRegister.text = intent.getStringExtra("nickname")
+
+        // 나중에 하기 클릭
+        binding.linearlayoutLaterRegister.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 시작하기 버튼 클릭
+        binding.btnStartRegister.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
