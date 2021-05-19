319c719af174cc225d1c67ed57be9345a63d8377
aerim <aerimforest98@gmail.com>
2021-05-19 10:17:42 +0900

    statue bar 색상 변경

--- app/src/main/java/com/example/happylife/SplashActivity.kt
+++ app/src/main/java/com/example/happylife/SplashActivity.kt
@@ -5,13 +5,19 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_splash.*
-
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // status bar 색상 변경
+        val window = this.window
+        window.statusBarColor = ContextCompat.getColor(this, R.color.main_color)
+        // statue bar 아이콘 색상 변경
+        window.decorView.systemUiVisibility = 0
 
         val anim_we = AnimationUtils.loadAnimation(this, R.anim.alpha_we)
         val anim_go = AnimationUtils.loadAnimation(this, R.anim.alpha_got)
