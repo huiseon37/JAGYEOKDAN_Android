6254734b205d35313cb102177f3b5c96c07dc6d3
rlatp1409 <stanley710@naver.com>
2021-05-21 03:13:19 +0900

    카테고리 자격증 리스트 화면 View 구현

---
+++ app/src/main/java/com/example/happylife/LicenseListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.happylife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LicenseListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license_list)
+    }
+}
