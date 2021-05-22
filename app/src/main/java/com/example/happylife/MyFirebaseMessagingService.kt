ae2ee4f6d135ceb6b834e47af550c6fbd6750231
aerim <aerimforest98@gmail.com>
2021-05-23 02:04:23 +0900

    푸시 알림 기능 구현

---
+++ app/src/main/java/com/example/happylife/MyFirebaseMessagingService.kt
@@ -0,0 +1,39 @@
+package com.example.happylife
+
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(p0: RemoteMessage) {
+        @SuppressLint("LongLogTAG")
+        if (p0.notification != null) {
+            sendNotification(p0.notification?.title, p0.notification!!.body!!)
+        }
+    }
+
+    private fun sendNotification(title: String?, body: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setSound(defaultSound)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
