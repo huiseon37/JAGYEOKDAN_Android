b4cde39f90135f3158079d0ebeb0848f41cc7af7
aerim <aerimforest98@gmail.com>
2021-05-22 17:25:52 +0900

    후기 작성 페이지 별점 기능 추가

--- app/src/main/java/com/example/happylife/WriteReviewActivity.kt
+++ app/src/main/java/com/example/happylife/WriteReviewActivity.kt
@@ -2,10 +2,36 @@ package com.example.happylife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happylife.databinding.ActivityWriteReviewBinding
 
 class WriteReviewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWriteReviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write_review)
+        binding = ActivityWriteReviewBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.star1.setOnClickListener {
+            binding.star1.setBackgroundResource(R.drawable.star_purple)
+        }
+
+        binding.star2.setOnClickListener {
+            binding.star2.setBackgroundResource(R.drawable.star_purple)
+        }
+
+        binding.star3.setOnClickListener {
+            binding.star3.setBackgroundResource(R.drawable.star_purple)
+        }
+
+        binding.star4.setOnClickListener {
+            binding.star4.setBackgroundResource(R.drawable.star_purple)
+        }
+
+        binding.star5.setOnClickListener {
+            binding.star5.setBackgroundResource(R.drawable.star_purple)
+        }
     }
 }
