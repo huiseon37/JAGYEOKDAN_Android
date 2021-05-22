6a41823d68317493046af98726138c3b1cc10d18
aerim <aerimforest98@gmail.com>
2021-05-23 04:24:44 +0900

    자격증 정보 화면 탭 이동시 시험 정보 변경

--- app/src/main/java/com/example/happylife/LicenseDetailInfo.kt
+++ app/src/main/java/com/example/happylife/LicenseDetailInfo.kt
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.happylife.databinding.ActivityLicenseDetailInfoBinding
+import com.example.happylife.model.CertificateInfoData
+import com.google.firebase.database.*
 
 class LicenseDetailInfo : AppCompatActivity() {
 
     private lateinit var binding: ActivityLicenseDetailInfoBinding
+
+    private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private var databaseReference: DatabaseReference = firebaseDatabase.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +34,10 @@ class LicenseDetailInfo : AppCompatActivity() {
 
         // 신청 사이트 버튼 클릭
         binding.btnRegisterSite.setOnClickListener {
-            val url = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.q-net.or.kr/crf005.do?id=crf00505&jmCd=1320"))
+            val url = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.q-net.or.kr/crf005.do?id=crf00505&jmCd=1320")
+            )
             startActivity(url)
         }
 
@@ -37,6 +45,11 @@ class LicenseDetailInfo : AppCompatActivity() {
         binding.btnQuestionSite.setOnClickListener {
             val url = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.comcbt.com/xe/j4"))
             startActivity(url)
+        }
+
+        // 1회 실기 버튼 클릭
+        binding.roundsSecond.setOnClickListener {
+            changeInfo()
         }
     }
 
@@ -46,5 +59,39 @@ class LicenseDetailInfo : AppCompatActivity() {
         binding.tvCommentLicenseDetail.visibility = View.INVISIBLE
         binding.tvComment2LicenseDetail.visibility = View.INVISIBLE
         binding.usePoint.visibility = View.INVISIBLE
+    }
+
+    // 자격증 정보
+    private fun changeInfo() {
+
+        // DB 테이블 연결
+        databaseReference = firebaseDatabase.getReference("certificate/it/정보처리기사")
+
+        var applyPeriod: String
+
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (postSnapshot in dataSnapshot.children) {
+                    if (postSnapshot.key == "1회 실기") {
+                        val item = postSnapshot.getValue(CertificateInfoData::class.java)
+                        if (item != null) {
+                            val startDate = item.examStartRegDate
+                            val endDate = item.examEndRegDate
+                            val cost = "${item.cost} 원"
+                            applyPeriod = "$startDate ~ $endDate"
+
+                            binding.applyDate.text = applyPeriod
+                            binding.testDate.text = item.examDate
+                            binding.resultDate.text = item.passStartDate
+                            binding.applyCost.text = cost
+                            binding.applyQualification.text = item.qualification.replace("bb", "\n")
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
     }
 }
--- app/src/main/java/com/example/happylife/model/CertificateInfoData.kt
+++ app/src/main/java/com/example/happylife/model/CertificateInfoData.kt
@@ -1,5 +1,12 @@
 package com.example.happylife.model
 
 data class CertificateInfoData(
-    val examStartRegDate: String = ""
+    val cost: String = "",
+    val enrollSite: String = "",
+    val examDate: String = "",
+    val examEndRegDate: String = "",
+    val examStartRegDate: String = "",
+    val passStartDate: String = "",
+    val qualification: String = "",
+    val testSite: String = ""
 )
