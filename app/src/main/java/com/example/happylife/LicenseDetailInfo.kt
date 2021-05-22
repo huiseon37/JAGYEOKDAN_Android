e3e76069e3cc9e50773e70955a269c63f1474472
aerim <aerimforest98@gmail.com>
2021-05-23 02:46:01 +0900

    포인트 사용한 리뷰 조회 기능 추가, 웹사이트 연결

--- app/src/main/java/com/example/happylife/LicenseDetailInfo.kt
+++ app/src/main/java/com/example/happylife/LicenseDetailInfo.kt
@@ -1,7 +1,10 @@
 package com.example.happylife
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.happylife.databinding.ActivityLicenseDetailInfoBinding
 
 class LicenseDetailInfo : AppCompatActivity() {
@@ -14,12 +17,34 @@ class LicenseDetailInfo : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        // 포인트 사용 버튼 클릭
         binding.usePoint.setOnClickListener {
             val dlg = UsePointDialog(this)
 
             dlg.setOnOKClickedListener {
+                openReview()
             }
             dlg.start()
         }
+
+        // 신청 사이트 버튼 클릭
+        binding.btnRegisterSite.setOnClickListener {
+            val url = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.q-net.or.kr/crf005.do?id=crf00505&jmCd=1320"))
+            startActivity(url)
+        }
+
+        // 기출 사이트 버튼 클릭
+        binding.btnQuestionSite.setOnClickListener {
+            val url = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.comcbt.com/xe/j4"))
+            startActivity(url)
+        }
+    }
+
+    // 리뷰 1개 오픈
+    private fun openReview() {
+        binding.shadeView.visibility = View.INVISIBLE
+        binding.tvCommentLicenseDetail.visibility = View.INVISIBLE
+        binding.tvComment2LicenseDetail.visibility = View.INVISIBLE
+        binding.usePoint.visibility = View.INVISIBLE
     }
 }
