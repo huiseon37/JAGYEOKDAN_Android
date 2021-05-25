c62c6a00206c79ef307baac22a329ce2f2e08b5a
rlatp1409 <stanley710@naver.com>
2021-05-25 15:44:50 +0900

    [] talktalk 액티비티

---
+++ app/src/main/java/com/example/happylife/TalkTalkActivity.kt
@@ -0,0 +1,11 @@
+package com.example.happylife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TalkTalkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_talk_talk)
+    }
+}
